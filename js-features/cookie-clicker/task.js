cf9f2e21e54d7456090f28cf51a93625bd283ab8
vels <velsymir@gmail.com>
2023-06-08 11:54:44 +0300

    clicker is ready

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,17 @@
 let cookie = document.getElementById('cookie');
 let count = document.getElementById('clicker__counter');
+let countTime = document.getElementById('clicker_time');
+let timer = Date.now();
 
 cookie.addEventListener('click', () => {
+
   if(cookie.width === 300) {
     cookie.width = 200
   } else {
     cookie.width = 300
   }
+
   ++count.textContent
+  countTime.textContent =(1/((Date.now()- timer)/1000)).toFixed(2)
+  timer = Date.now()
 })
