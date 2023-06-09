1870175f8f19d5b718db41e65a7088f89a287e79
vels <velsymir@gmail.com>
2023-06-09 21:25:41 +0300

    add homeworks

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,22 +1,59 @@
-(() => {
-  let playing = true,
-    activeHole = 1;
+let playing = true,
+  activeHole = 1;
+
+let start = () => {
 
   const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
+        time = prompt('сложность игры', 1000)
+        getHole = index => document.getElementById(`hole${index}`),
+        deactivateHole = index => getHole( index ).className = 'hole',
+        activateHole = index => getHole( index ).className = 'hole hole_has-mole',
+        next = () => setTimeout(() => {
+          if ( !playing ) {
+            return;
+            }
+          deactivateHole( activeHole );
+          activeHole = Math.floor( 1 + Math.random() * 9 );
+          activateHole( activeHole );
+          next();
+        }, time );
 
   next();
-})();
+}
+
+start()
+
+function getHole(index) {
+  return document.getElementById(`hole${index}`)
+}
+let count=0,
+    missCount=0;
+
+for(let i = 1; i < 10; i++) {
+  getHole(i).addEventListener('click', ()=>{
+    if (getHole(i).className.includes('hole_has-mole')) {
+      count++
+    } else {
+      missCount++
+    }
+    document.getElementById('dead').textContent = count
+    document.getElementById('lost').textContent = missCount
+    if(count > 3) {
+      document.getElementById('dead').textContent = 0;
+      document.getElementById('lost').textContent = 0;
+      alert('win');
+      missCount = 0;
+      count = 0;
+      start()
+    }
+    if(missCount > 5) {
+      document.getElementById('dead').textContent = 0;
+      document.getElementById('lost').textContent = 0;
+      alert('game over');
+      missCount = 0;
+      count = 0;
+      start()
+    }
+  })
+
+}
